dea07e0257408d9ada936027d14f9980c94b918e
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-04-20 00:04:19 +0530

    Database connectivity

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path: './.env'
+});
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`The Server is running at port: ${process.env.PORT}`);
+    });
+}).catch((err) => {
+    console.log("Mongo DB connection failed:", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import  express  from 'express';
+const app = express()
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error", (error) => {
+        console.log("Error", error);
+        throw error
+       })
+
+       app.listen(process.env.PORT, () => {
+        console.log(`Server is listening at ${process.env.PORT}`);
+       })
+
+    } catch (error) {
+        console.error("Error:", error);
+        throw err;
+    }
+})();
+*/
